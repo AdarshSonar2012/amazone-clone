09b837651736f47c010166a26950014798aba69a
Adarsh Sonar <adarshsonar2012@gmail.com>
2023-07-17 18:56:03 +0530

    deployed in firebase

--- src/Checkout.js
+++ src/Checkout.js
@@ -16,7 +16,7 @@ function Checkout() {
           src="https://images-eu.ssl-images-amazon.com/images/G/31/AmazonKarigar/Saheli_store_banner.png"
         />
         <div>
-          <h3>Hello,{user?.email}</h3>
+          <h3>Hello,{user && user.email}</h3>
           {/* {user?.email ? <h3>user.email</h3> : <h3>hello</h3>} */}
           <h2 className="checkout-title">Your Shopping basket</h2>
           {basket.map((item) => (
