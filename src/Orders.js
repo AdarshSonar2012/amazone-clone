bfc946630073817f51b7302fb6788586a7863874
Adarsh Sonar <adarshsonar2012@gmail.com>
2023-08-02 23:44:41 +0530

    latest

--- src/Orders.js
+++ src/Orders.js
@@ -1,10 +1,43 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
 import './Orders.css'
+import { db } from './Firebase';
+import {query, collection, onSnapshot, orderBy, doc} from 'firebase/firestore'
+import Order from './Order';
+
+import CheckoutProduct from './CheckoutProduct'
+import { useStateValue } from './StateProvider';
 
 function Orders() {
+  const [{ basket, user }, dispatch] = useStateValue();
+  const [orders,setOrders]= useState([]);
+
+  useEffect(()=>{
+    if(user){
+    const ref = collection(db, 'users',user.uid, 'orders');
+    const orderedOrders = query(ref, orderBy('created', 'desc'))
+    onSnapshot(orderedOrders, snapshot => {
+
+        setOrders(snapshot.docs.map(doc => ({
+            id: doc.id,
+            data: doc.data()
+        })))
+      })
+      console.log(doc.data);
+    }else{
+      setOrders([])
+    }
+  },[user]);
+
+
+
   return (
     <div className='orders'>
       <h1>Your Orders</h1>
+      <div className='orders-order'>
+        {orders && orders.map((order) => (
+         <Order order={order}/>
+        ))}
+      </div>
     </div>
   )
 }
